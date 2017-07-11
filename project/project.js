f1fea17e53b92605fe8b42054773d9b4d923f866
Chris <ulrich@tcs-germany.de>
2017-07-11 15:35:44 +0200

    Start to implement Project List

--- project/project.js
+++ project/project.js
@@ -1,21 +1,40 @@
-var siTimes,tbToolbar,gTimes,dsTimes;
+function RefreshProjects() {
+  siProject.progressOn();
+  try {
+    console.log("Refresh Times");
+    dsProjects.FillGrid(gProjects,'');
+  } finally {
+    siProject.progressOff();
+  }
+}
+var siProject,tbToolbar,gProjects,dsProjects;
 dhtmlxEvent(window,"load",function(){
   console.log("Loading Projects Page...");
   sbMain.addItem({id: 'siProject', text: 'Projekte', icon: 'fa fa-refresh'});
   siProject = window.parent.sbMain.cells('siProject');
-  tbToolbar = siTimes.attachToolbar({
+  tbToolbar = siProject.attachToolbar({
     parent:"pToolbar",
       items:[
-         {id: "new", type: "button", text: "Neu", img: "fa fa-plus-circle"}
-        ,{id: "sep1", type: "separator" }
-        ,{id: "sep1", type: "separator" }
-        ,{id: "refresh", type: "button", text: "Aktualisieren", img: "fa fa-refresh"}
+        {id: "refresh", type: "button", text: "Aktualisieren", img: "fa fa-refresh"}
       ],
     iconset: "awesome"
   });
   tbToolbar.attachEvent("onClick", function(id) {
     if (id=='new') {
     } else if (id=='refresh') {
+      RefreshProjects();
     }
   });
+  gProjects = siProject.attachGrid({parent:"pTimes"});
+  gProjects.setImagePath("codebase/imgs/");
+  //gTimes.enableAutoWidth(true);
+  //gTimes.enableAutoHeight(true);
+  gProjects.setSizes();
+  gProjects.setHeader(["Nummer","Name","Status","Kategorie"]);
+  gProjects.setColumnIds('ID,NAME,STATUS,CATEGORY')
+  gProjects.setColTypes("txt,txt,txt,txt");
+  //gTimes.enableEditEvents(false,true,true);
+  gProjects.init();
+  dsProjects = newPrometDataStore('projects');
+  dsProjects.DataProcessor.init(gProjects);
 });

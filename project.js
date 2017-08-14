a170d11383b17faccb4cf9405e142a4939697001
Christian Ulrich <info@cu-tec.de>
2017-08-14 18:23:27 +0200

    Move to Form Classes

--- project.js
+++ project.js
@@ -1,56 +1,9 @@
-function RefreshProjects() {
-  siProject.progressOn();
-  try {
-    console.log("Refresh Times");
-    dsProjects.FillGrid(gProjects,'',0,function (){
-      siProject.progressOff();
-    });
-  } catch(err) {
-    console.log('Refresh Exception:'+err.message);
-    siProject.progressOff();
-  }
-}
-var siProject,tbToolbar,gProjects,dsProjects;
+var Projects;
 dhtmlxEvent(window,"load",function(){
-  console.log("Loading Projects Page...");
-  sbMain.addItem({id: 'siProject', text: 'Projekte', icon: 'fa fa-refresh'});
-  siProject = window.parent.sbMain.cells('siProject');
-  tbToolbar = siProject.attachToolbar({
-    parent:"pToolbar",
-      items:[
-        {id: "refresh", type: "button", text: "Aktualisieren", img: "fa fa-refresh"}
-      ],
-    iconset: "awesome"
-  });
-  tbToolbar.attachEvent("onClick", function(id) {
-    if (id=='new') {
-    } else if (id=='refresh') {
-      RefreshProjects();
-    }
-  });
-  gProjects = siProject.attachGrid({parent:"pTimes"});
-  gProjects.setImagePath("codebase/imgs/");
-  gProjects.setSizes();
-  gProjects.setHeader(["Nummer","Name","Status","Kategorie"]);
-  gProjects.setColumnIds('ID,NAME,STATUS,CATEGORY')
-  gProjects.setColTypes("txt,txt,txt,txt");
-  gProjects.attachHeader("#text_filter,#text_filter,#select_filter,");
-  //gProjects.filterByAll=function(){
-  //};
-  gProjects.attachEvent("onFilterStart", function(indexes,values){
-    var aFilter = '';
-    for (var i = 0; i < indexes.length; i++) {
-      if (values[i]!='')
-        aFilter += ' AND "'+gProjects.getColumnId(indexes[i])+'"'+' like \'%'+values[i]+'%\'';
-    }
-    aFilter = aFilter.substring(5,aFilter.length);
-    siProject.progressOn();
-    dsProjects.FillGrid(gProjects,aFilter,0,function (){
-      siProject.progressOff();
-    });
-    return false;
-  });
-  gProjects.init();
-  dsProjects = newPrometDataStore('projects');
-  dsProjects.DataProcessor.init(gProjects);
+  Projects = newPrometList('projects','Projekte');
+  Projects.Grid.setHeader(["Nummer","Name","Status","Kategorie"]);
+  Projects.Grid.setColumnIds('ID,NAME,STATUS,CATEGORY')
+  Projects.Grid.setColTypes("txt,txt,txt,txt");
+  Projects.Grid.attachHeader("#text_filter,#text_filter,#select_filter,");
+  Projects.Grid.init();
 });

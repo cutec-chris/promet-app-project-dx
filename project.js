cac978fc374cbc20698d056e2f45ccdd467b5615
Christian Ulrich <info@cu-tec.de>
2017-08-15 17:28:44 +0200

    better Column Widths

--- project.js
+++ project.js
@@ -1,9 +1,10 @@
 var Projects;
 dhtmlxEvent(window,"load",function(){
   Projects = newPrometList('projects','Projekte');
-  Projects.Grid.setHeader(["Nummer","Name","Status","Kategorie"]);
-  Projects.Grid.setColumnIds('ID,NAME,STATUS,CATEGORY')
+  Projects.Grid.setHeader(["Name","Nummer","Status","Kategorie"]);
+  Projects.Grid.setColumnIds('NAME,ID,STATUS,CATEGORY')
   Projects.Grid.setColTypes("txt,txt,txt,txt");
   Projects.Grid.attachHeader("#text_filter,#text_filter,#select_filter,");
+  Meetings.Grid.setInitWidths('*,70,150,*');
   Projects.Grid.init();
 });

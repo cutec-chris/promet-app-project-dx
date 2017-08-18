d201df7bd09f1a9cef70be5d96627434f90a2286
Christian Ulrich <info@cu-tec.de>
2017-08-18 05:22:31 +0200

    make columns read only

--- project.js
+++ project.js
@@ -3,7 +3,7 @@ dhtmlxEvent(window,"load",function(){
   Projects = newPrometList('projects','Projekte');
   Projects.Grid.setHeader(["Name","Nummer","Status","Kategorie"]);
   Projects.Grid.setColumnIds('NAME,ID,STATUS,CATEGORY')
-  Projects.Grid.setColTypes("txt,txt,txt,txt");
+  Projects.Grid.setColTypes("ro,ro,ro,ro");
   Projects.Grid.attachHeader("#text_filter,#text_filter,#select_filter,");
   Meetings.Grid.setInitWidths('*,70,150,*');
   Projects.Grid.init();

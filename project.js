be8b175917482a90e7a5087d582c02b63477a9fb
Chris <ulrich@tcs-germany.de>
2018-01-09 15:16:32 +0100

    Fix Project List

--- project.js
+++ project.js
@@ -5,6 +5,6 @@ dhtmlxEvent(window,"load",function(){
   Projects.Grid.setColumnIds('NAME,ID,STATUS,CATEGORY')
   Projects.Grid.setColTypes("ro,ro,ro,ro");
   Projects.Grid.attachHeader("#text_filter,#text_filter,#select_filter,");
-  Meetings.Grid.setInitWidths('*,70,150,*');
+  Projects.Grid.setInitWidths('*,70,150,*');
   Projects.Grid.init();
 });

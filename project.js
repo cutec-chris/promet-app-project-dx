96b860f8e2ed8da98f19dff1fd34c5e9df1ae75d
Chris <ulrich@tcs-germany.de>
2018-01-10 14:53:31 +0100

    started Adding Gantt

--- project.js
+++ project.js
@@ -4,7 +4,45 @@ dhtmlxEvent(window,"load",function(){
   Projects.Grid.setHeader(["Name","Nummer","Status","Kategorie"]);
   Projects.Grid.setColumnIds('NAME,ID,STATUS,CATEGORY')
   Projects.Grid.setColTypes("ro,ro,ro,ro");
-  Projects.Grid.attachHeader("#text_filter,#text_filter,#select_filter,");
-  Projects.Grid.setInitWidths('*,70,150,*');
+  Projects.Grid.attachHeader("#text_filter,#text_filter,#select_filter,#text_filter");
+  Projects.Grid.setInitWidths('*,100,150,100,200');
   Projects.Grid.init();
+  Projects.OnCreateForm = function(aForm) {
+    aForm.Tabs.addTab(
+    "plan",       // id
+    "Plan",    // tab text
+    null,       // auto width
+    null,       // last position
+    false,      // inactive
+    false);
+    var aDiv = document.createElement('div');
+    aForm.Parent.appendChild(aDiv);
+    aForm.Tabs.tabs("plan").attachObject(aDiv);
+    aDiv.style.height = '100%';
+    aDiv.style.width = '100%';
+    aForm.OnDataUpdated = function(bForm) {
+      gantt.config.xml_date="%Y-%m-%d %H:%i";
+      gantt.init(aDiv);
+      for (var i = 0; i < bForm.Data.TASKS.length; i++) {
+        var aObj = {};
+        if (bForm.Data.TASKS[i].Fields.active != 0) {
+          aObj.id = bForm.Data.TASKS[i].Fields.sql_id;
+          aObj.text = bForm.Data.TASKS[i].Fields.summary;
+          if (bForm.Data.TASKS[i].Fields.startdate) {
+            var aDate = new Date(bForm.Data.TASKS[i].Fields.startdate);
+            aObj.start_date = formatDate(aDate,"YYYY-MM-dd");
+          } else {
+            var aDate = new Date();
+            aObj.start_date = formatDate(aDate,"YYYY-MM-dd");
+          }
+          if (bForm.Data.TASKS[i].Fields.plantime>0)
+            aObj.duration = bForm.Data.TASKS[i].Fields.plantime * 8;
+          if ((bForm.Data.TASKS[i].Fields.parent)&&(bForm.Data.TASKS[i].Fields.parent!="0"))
+            aObj.parent = parseInt(bForm.Data.TASKS[i].Fields.parent);
+          gantt.addTask(aObj);
+        }
+      }
+      //gantt.parse(tasks);
+    }
+  }
 });

7b2968e1d5f4b5a403f32444220e8fb6aa3c38ae
Chris <ulrich@tcs-germany.de>
2018-01-11 08:57:01 +0100

    Move Gantt to Button/separate Window (not jet ready)

--- project.js
+++ project.js
@@ -8,41 +8,38 @@ dhtmlxEvent(window,"load",function(){
   Projects.Grid.setInitWidths('*,100,150,100,200');
   Projects.Grid.init();
   Projects.OnCreateForm = function(aForm) {
-    aForm.Tabs.addTab(
-    "plan",       // id
-    "Plan",    // tab text
-    null,       // auto width
-    null,       // last position
-    false,      // inactive
-    false);
-    var aDiv = document.createElement('div');
-    aForm.Parent.appendChild(aDiv);
-    aForm.Tabs.tabs("plan").attachObject(aDiv);
-    aDiv.style.height = '100%';
-    aDiv.style.width = '100%';
-    aForm.OnDataUpdated = function(bForm) {
-      gantt.config.xml_date="%Y-%m-%d %H:%i";
-      gantt.init(aDiv);
-      for (var i = 0; i < bForm.Data.TASKS.length; i++) {
-        var aObj = {};
-        if (bForm.Data.TASKS[i].Fields.active != 0) {
-          aObj.id = bForm.Data.TASKS[i].Fields.sql_id;
-          aObj.text = bForm.Data.TASKS[i].Fields.summary;
-          if (bForm.Data.TASKS[i].Fields.startdate) {
-            var aDate = new Date(bForm.Data.TASKS[i].Fields.startdate);
-            aObj.start_date = formatDate(aDate,"YYYY-MM-dd");
-          } else {
-            var aDate = new Date();
-            aObj.start_date = formatDate(aDate,"YYYY-MM-dd");
+    aForm.Toolbar.addButton('gantt', 3, 'Gantt', 'fa fa-bar-chart fa-rotate-90');
+    aForm.Toolbar.attachEvent("onClick", function(id) {
+      if (id=='gantt') {
+        var aDiv = document.createElement('div');
+        aForm.Parent.appendChild(aDiv);
+        aForm.Tabs.tabs("plan").attachObject(aDiv);
+        aDiv.style.height = '100%';
+        aDiv.style.width = '100%';
+        aForm.OnDataUpdated = function(bForm) {
+          gantt.config.xml_date="%Y-%m-%d %H:%i";
+          gantt.init(aDiv);
+          for (var i = 0; i < bForm.Data.TASKS.length; i++) {
+            var aObj = {};
+            if (bForm.Data.TASKS[i].Fields.active != 0) {
+              aObj.id = bForm.Data.TASKS[i].Fields.sql_id;
+              aObj.text = bForm.Data.TASKS[i].Fields.summary;
+              if (bForm.Data.TASKS[i].Fields.startdate) {
+                var aDate = new Date(bForm.Data.TASKS[i].Fields.startdate);
+                aObj.start_date = formatDate(aDate,"YYYY-MM-dd");
+              } else {
+                var aDate = new Date();
+                aObj.start_date = formatDate(aDate,"YYYY-MM-dd");
+              }
+              if (bForm.Data.TASKS[i].Fields.plantime>0)
+                aObj.duration = bForm.Data.TASKS[i].Fields.plantime * 8;
+              if ((bForm.Data.TASKS[i].Fields.parent)&&(bForm.Data.TASKS[i].Fields.parent!="0"))
+                aObj.parent = parseInt(bForm.Data.TASKS[i].Fields.parent);
+              gantt.addTask(aObj);
+            }
           }
-          if (bForm.Data.TASKS[i].Fields.plantime>0)
-            aObj.duration = bForm.Data.TASKS[i].Fields.plantime * 8;
-          if ((bForm.Data.TASKS[i].Fields.parent)&&(bForm.Data.TASKS[i].Fields.parent!="0"))
-            aObj.parent = parseInt(bForm.Data.TASKS[i].Fields.parent);
-          gantt.addTask(aObj);
+          //gantt.parse(tasks);
         }
-      }
-      //gantt.parse(tasks);
-    }
+      }});
   }
 });

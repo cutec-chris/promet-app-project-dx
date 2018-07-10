b6fe9e9cc22a4bbcc9a81ed81e6669f1bede3135
Chris <ulrich@tcs-germany.de>
2018-07-10 12:55:50 +0200

    Trial to implement Gantt Window

--- projects.js
+++ projects.js
@@ -1,7 +1,163 @@
-﻿rtl.module("projects",["System","JS","Web","Classes","Avamm","webrouter","AvammForms","dhtmlx_base","SysUtils"],function () {
+﻿rtl.module("projects",["System","JS","Web","Classes","Avamm","webrouter","AvammForms","dhtmlx_base","SysUtils","Types"],function () {
   "use strict";
   var $mod = this;
   rtl.createClass($mod,"TProjectForm",pas.AvammForms.TAvammForm,function () {
+    this.$init = function () {
+      pas.AvammForms.TAvammForm.$init.call(this);
+      this.FGanttWindow = null;
+      this.Tasks = null;
+    };
+    this.$final = function () {
+      this.FGanttWindow = undefined;
+      this.Tasks = undefined;
+      pas.AvammForms.TAvammForm.$final.call(this);
+    };
+    this.GanttWindowLoaded = function (Event) {
+      var Result = false;
+      var aDiv = null;
+      var aObj = null;
+      var aLink = null;
+      var aDate = 0.0;
+      var aDep = null;
+      var i = 0;
+      var a = 0;
+      var newWindow = null;
+      pas.System.Writeln("loaded called ...");
+      newWindow = this.FGanttWindow;
+      aDiv = this.FGanttWindow.document.createElement("div");
+      this.FGanttWindow.document.body.appendChild(aDiv);
+      aDiv.style.setProperty("height","100%");
+      aDiv.style.setProperty("width","100%");
+      //gantt config
+          //newWindow.Form = aForm;
+          newWindow.window.gantt.templates.scale_cell_class = function(date){
+              if(date.getDay()==0||date.getDay()==6){
+                  return "weekend";
+              }
+          };
+          newWindow.window.gantt.templates.task_cell_class = function(item,date){
+              if(date.getDay()==0||date.getDay()==6){
+                  return "weekend" ;
+              }
+          };
+          newWindow.window.gantt.config.drag_progress = false;
+          //newWindow.window.gantt.config.date_scale = "Week #%W";
+          //newWindow.window.gantt.config.scale_unit= "week";
+          newWindow.window.gantt.config.date_scale = "%d";
+          newWindow.window.gantt.config.scale_unit= "day";
+          newWindow.window.gantt.config.step = 1;
+          newWindow.window.gantt.config.fit_tasks = true;
+          newWindow.window.gantt.config.subscales = [
+            {unit:"week", step:1, date:"KW %W" },
+                {unit:"month", step:1, date:"%M" }
+                //{unit:"year", step:1, date:"%Y" }
+            ];
+          newWindow.window.gantt.config.work_time = true;
+          newWindow.window.gantt.config.correct_work_time = true;
+          newWindow.window.gantt.config.columns = [
+            { name: "text", label: "Aufgabe", tree: true, width: 300 },
+            { name: "start_date", label: "Start", align: "center" },
+            { name:"duration", label: "Dauer",align: "center", width:70 },
+            { name: "add", label: "", width: 44 }
+            ];
+          newWindow.window.gantt.config.scale_height = 3*28;
+      
+          newWindow.window.gantt.config.task_height = 16;
+            newWindow.window.gantt.config.row_height = 20;
+      
+          newWindow.window.gantt.config.xml_date="%Y-%m-%d %H:%i";
+          newWindow.window.gantt.init(aDiv,new Date().addDays(-5),new Date().addDays(90));
+      for (var $l1 = 0, $end2 = this.Tasks.length; $l1 <= $end2; $l1++) {
+        i = $l1;
+        aObj = new Object();
+        var $with3 = rtl.getObject(this.Tasks[i]);
+        if ($with3["ACTIVE"] != 0) {
+          aObj["id"] = $with3["sql_id"];
+          aObj["text"] = $with3["SUMMARY"];
+          if (pas.System.Assigned($with3["STARTDATE"])) {
+            aDate = pas.SysUtils.StrToDateTime("" + $with3["STARTDATE"]);
+            aObj["start_date"] = pas.SysUtils.FormatDateTime("YYYY-MM-dd",aDate);
+          } else {
+            aObj["start_date"] = pas.SysUtils.FormatDateTime("YYYY-MM-dd",pas.SysUtils.Now());
+          };
+          if (Math.floor($with3["plantime"]) > 0) aObj["duration"] = Math.floor($with3["plantime"]) * 8;
+          if (pas.System.Assigned($with3["parent"]) && ($with3["parent"] != "0")) aObj["parent"] = Math.floor($with3["parent"]);
+          newWindow.window.gantt.addTask(aObj);
+        };
+      };
+      for (var $l4 = 0, $end5 = this.Tasks.length; $l4 <= $end5; $l4++) {
+        i = $l4;
+        aObj = new Object();
+        aLink = new Object();
+        var $with6 = rtl.getObject(this.Tasks[i]);
+        if ($with6["ACTIVE"] != 0) {
+          aObj["id"] = $with6["sql_id"];
+          aObj["text"] = $with6["SUMMARY"];
+          if (pas.System.Assigned($with6["STARTDATE"])) {
+            aDate = pas.SysUtils.StrToDateTime("" + $with6["STARTDATE"]);
+            aObj["start_date"] = pas.SysUtils.FormatDateTime("YYYY-MM-dd",aDate);
+          } else {
+            aObj["start_date"] = pas.SysUtils.FormatDateTime("YYYY-MM-dd",pas.SysUtils.Now());
+          };
+          if (Math.floor($with6["plantime"]) > 0) aObj["duration"] = Math.floor($with6["plantime"]) * 8;
+          if (pas.System.Assigned($with6["parent"]) && ($with6["parent"] != "0")) aObj["parent"] = Math.floor($with6["parent"]);
+          newWindow.window.gantt.addTask(aObj);
+          if (pas.System.Assigned($with6["DEPENDENCIES"])) {
+            aDep = rtl.getObject(rtl.getObject($with6["DEPENDENCIES"])["Data"]);
+            for (var $l7 = 0, $end8 = aDep.length - 1; $l7 <= $end8; $l7++) {
+              a = $l7;
+              aLink["id"] = rtl.getObject(aDep[a])["sql_id"];
+              aLink["source"] = rtl.getObject(aDep[a])["ref_id_id"];
+              aLink["target"] = $with6["sql_id"];
+              aLink.type = newWindow.window.gantt.config.links.finish_to_start;
+              newWindow.window.gantt.addLink(aLink);
+            };
+          };
+        };
+      };
+      return Result;
+    };
+    this.GanttWindowLoadend = function (aEvent) {
+      var Result = false;
+      this.GanttWindowLoaded(rtl.getObject(null));
+      return Result;
+    };
+    this.DoLoadData = function () {
+      pas.AvammForms.TAvammForm.DoLoadData.call(this);
+      this.DoCreate();
+      this.DoOpen();
+    };
+    this.ToolbarButtonClick = function (id) {
+      if (id === "gantt") {
+        this.ShowGantt();
+      };
+    };
+    this.ShowGantt = function () {
+      var newPath = "";
+      var pathArray = [];
+      var i = 0;
+      this.FGanttWindow = window.open("","_blank");
+      if (this.FGanttWindow != null) {
+        pathArray = window.location.pathname.split("\/");
+        for (var $l1 = 0, $end2 = rtl.length(pathArray) - 1; $l1 <= $end2; $l1++) {
+          i = $l1;
+          newPath = newPath + "\/";
+          newPath = newPath + pathArray[i];
+        };
+        this.FGanttWindow.location.href = (((window.location.protocol + "\/\/") + window.location.host) + newPath) + "projects\/gantt.html";
+        this.FGanttWindow.onload=this.GanttWindowLoaded;
+        this.FGanttWindow.onloadend = rtl.createCallback(this,"GanttWindowLoadend");
+      };
+    };
+    this.DoCreate = function () {
+      this.Toolbar.addButton("gantt",3,rtl.getResStr(pas.projects,"strGantt"),"fa fa-bar-chart fa-rotate-90");
+      this.Toolbar.attachEvent("onClick",rtl.createCallback(this,"ToolbarButtonClick"));
+    };
+    this.DoOpen = function () {
+      if (rtl.isExt(this.FData["TASKS"],Object,1)) {
+        this.Tasks = rtl.getObject(rtl.getObject(this.FData["TASKS"])["Data"])}
+       else this.Tasks = rtl.getObject(this.FData["TASKS"]);
+    };
   });
   this.Project = null;
   this.ShowProject = function (URl, aRoute, Params) {
@@ -22,7 +178,7 @@
     };
     $mod.Project.Show();
   };
-  $mod.$resourcestrings = {strProject: {org: "Projekte"}};
+  $mod.$resourcestrings = {strProject: {org: "Projekte"}, strGantt: {org: "Gantt"}};
   $mod.$init = function () {
     if (pas.Avamm.getRight("Projects") > 0) pas.Avamm.RegisterSidebarRoute(rtl.getResStr(pas.projects,"strProject"),"projects",$mod.ShowProjectList);
     pas.webrouter.Router().RegisterRoute("\/projects\/by-id\/:Id\/",$mod.ShowProject,false);
--- projects.lpr
+++ projects.lpr
@@ -4,11 +4,26 @@ library projects;
 
 type
 
+  { TProjectForm }
+
   TProjectForm = class(TAvammForm)
+  private
+    FGanttWindow : TJSWindow;
+    function GanttWindowLoaded(Event: TEventListenerEvent): boolean;
+    function GanttWindowLoadend(aEvent: TJSLoadEvent): boolean;
+  protected
+    Tasks : TJSArray;
+    procedure DoLoadData; override;
+    procedure ToolbarButtonClick(id : string);
+    procedure ShowGantt;
+  public
+    procedure DoCreate;
+    procedure DoOpen;
   end;
 
 resourcestring
   strProject             = 'Projekte';
+  strGantt               = 'Gantt';
 
 var
   Project : TAvammListForm = nil;
@@ -36,6 +51,196 @@ begin
         end;
     end;
   Project.Show;
+end;
+
+{ TProjectForm }
+
+function TProjectForm.GanttWindowLoaded(Event: TEventListenerEvent): boolean;
+var
+  aDiv: TJSHTMLElement;
+  aObj, aLink: TJSObject;
+  aDate: TDateTime;
+  aDep: TJSArray;
+  i, a: Integer;
+  newWindow: TJSWindow;
+begin
+  writeln('loaded called ...');
+  newWindow := FGanttWindow;
+  aDiv := TJSHTMLElement(FGanttWindow.document.createElement('div'));
+  FGanttWindow.document.body.appendChild(aDiv);
+  aDiv.style.setProperty('height','100%');
+  aDiv.style.setProperty('width','100%');
+  asm //gantt config
+    //newWindow.Form = aForm;
+    newWindow.window.gantt.templates.scale_cell_class = function(date){
+        if(date.getDay()==0||date.getDay()==6){
+            return "weekend";
+        }
+    };
+    newWindow.window.gantt.templates.task_cell_class = function(item,date){
+        if(date.getDay()==0||date.getDay()==6){
+            return "weekend" ;
+        }
+    };
+    newWindow.window.gantt.config.drag_progress = false;
+    //newWindow.window.gantt.config.date_scale = "Week #%W";
+    //newWindow.window.gantt.config.scale_unit= "week";
+    newWindow.window.gantt.config.date_scale = "%d";
+    newWindow.window.gantt.config.scale_unit= "day";
+    newWindow.window.gantt.config.step = 1;
+    newWindow.window.gantt.config.fit_tasks = true;
+    newWindow.window.gantt.config.subscales = [
+      {unit:"week", step:1, date:"KW %W" },
+   		  {unit:"month", step:1, date:"%M" }
+  		  //{unit:"year", step:1, date:"%Y" }
+  	  ];
+    newWindow.window.gantt.config.work_time = true;
+    newWindow.window.gantt.config.correct_work_time = true;
+    newWindow.window.gantt.config.columns = [
+      { name: "text", label: "Aufgabe", tree: true, width: 300 },
+      { name: "start_date", label: "Start", align: "center" },
+      { name:"duration", label: "Dauer",align: "center", width:70 },
+      { name: "add", label: "", width: 44 }
+      ];
+    newWindow.window.gantt.config.scale_height = 3*28;
+
+    newWindow.window.gantt.config.task_height = 16;
+	  newWindow.window.gantt.config.row_height = 20;
+
+    newWindow.window.gantt.config.xml_date="%Y-%m-%d %H:%i";
+    newWindow.window.gantt.init(aDiv,new Date().addDays(-5),new Date().addDays(90));
+  end;
+
+  for i := 0 to Tasks.length do
+    begin
+      aObj := TJSObject.new;
+      with TJSObject(Tasks[i]) do
+        begin
+          if (Properties['ACTIVE'] <> 0) then
+            begin
+              aObj.Properties['id'] := Properties['sql_id'];
+              aObj.Properties['text'] := Properties['SUMMARY'];
+              if Assigned(Properties['STARTDATE']) then
+                begin
+                  aDate := StrToDateTime(string(Properties['STARTDATE']));
+                  aObj.Properties['start_date'] := FormatDateTime('YYYY-MM-dd',aDate);
+                end
+              else
+                begin
+                  aObj.Properties['start_date'] := FormatDateTime('YYYY-MM-dd',Now());
+                end;
+              if (Integer(Properties['plantime'])>0) then
+                aObj.Properties['duration'] := Integer(Properties['plantime']) * 8;
+              if (Assigned(Properties['parent']) and (Properties['parent']<>'0')) then
+                aObj.Properties['parent'] := Integer(Properties['parent']);
+              asm
+                newWindow.window.gantt.addTask(aObj);
+              end;
+            end;
+        end;
+    end;
+  for i := 0 to Tasks.length do
+    begin
+      aObj := TJSObject.new;
+      aLink := TJSObject.new;
+      with TJSObject(Tasks[i]) do
+        begin
+          if (Properties['ACTIVE'] <> 0) then
+            begin
+              aObj.Properties['id'] := Properties['sql_id'];
+              aObj.Properties['text'] := Properties['SUMMARY'];
+              if Assigned(Properties['STARTDATE']) then
+                begin
+                  aDate := StrToDateTime(string(Properties['STARTDATE']));
+                  aObj.Properties['start_date'] := FormatDateTime('YYYY-MM-dd',aDate);
+                end
+              else
+                begin
+                  aObj.Properties['start_date'] := FormatDateTime('YYYY-MM-dd',Now());
+                end;
+              if (Integer(Properties['plantime'])>0) then
+                aObj.Properties['duration'] := Integer(Properties['plantime']) * 8;
+              if (Assigned(Properties['parent']) and (Properties['parent']<>'0')) then
+                aObj.Properties['parent'] := Integer(Properties['parent']);
+              asm
+                newWindow.window.gantt.addTask(aObj);
+              end;
+              if Assigned(Properties['DEPENDENCIES']) then
+                begin
+                  aDep := TJSArray(TJSObject(Properties['DEPENDENCIES']).Properties['Data']);
+                  for a := 0 to aDep.Length-1 do
+                    begin
+                      aLink.Properties['id'] :=TJSObject(aDep[a]).Properties['sql_id'];
+                      aLink.Properties['source'] := TJSObject(aDep[a]).Properties['ref_id_id'];
+                      aLink.Properties['target'] := Properties['sql_id'];
+                      asm
+                        aLink.type = newWindow.window.gantt.config.links.finish_to_start;
+                        newWindow.window.gantt.addLink(aLink);
+                      end;
+                    end;
+                end;
+            end;
+        end;
+    end;
+end;
+
+function TProjectForm.GanttWindowLoadend(aEvent: TJSLoadEvent): boolean;
+begin
+  GanttWindowLoaded(TEventListenerEvent(null));
+end;
+
+procedure TProjectForm.DoLoadData;
+begin
+  inherited DoLoadData;
+  DoCreate;
+  DoOpen;
+end;
+
+procedure TProjectForm.ToolbarButtonClick(id: string);
+begin
+  if id='gantt' then
+    begin
+      ShowGantt;
+    end;
+end;
+
+procedure TProjectForm.ShowGantt;
+var
+  newPath: String;
+  pathArray: TStringDynArray;
+  i: Integer;
+
+begin
+  FGanttWindow := window.open('','_blank');
+  if Assigned(FGanttWindow) then //no rights to open an new window (possibly were running from file:// so we use an dhtmlx window)
+    begin
+      pathArray := TJSString(window.location.pathname).split( '/' );
+      for i := 0 to length(pathArray)-1 do
+        begin
+          newPath := newPath + '/';
+          newPath := newPath + pathArray[i];
+        end;
+      FGanttWindow.location.href:=window.location.protocol + '//' + window.location.host + newPath+'projects/gantt.html';
+      asm
+        this.FGanttWindow.onload=this.GanttWindowLoaded;
+      end;
+      FGanttWindow.onloadend:=@GanttWindowLoadend;
+    end;
+end;
+
+procedure TProjectForm.DoCreate;
+begin
+  Toolbar.addButton('gantt',3,strGantt,'fa fa-bar-chart fa-rotate-90');
+  Toolbar.attachEvent('onClick', @ToolbarButtonClick);
+end;
+
+procedure TProjectForm.DoOpen;
+begin
+  if Data.Properties['TASKS'] is TJSObject then
+    Tasks := TJSArray(TJSObject(Data.Properties['TASKS']).Properties['Data'])
+  else
+    Tasks := TJSArray(Data.Properties['TASKS']);
+
 end;
 
 initialization
